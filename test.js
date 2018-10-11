26ed814be97cb16207e688579d4d236960a180e1
Dario Vladovic <d.vladimyr@gmail.com>
2018-10-11 12:13:12 +0200

    Update test suite :tada:

--- test.js
+++ test.js
@@ -19,10 +19,19 @@ test('fetch latest haiku', async t => {
 });
 
 test('fetch random haiku', async t => {
-  t.plan(3);
+  t.plan(4);
   const haiku1 = await haikuist.fetchRandom();
   const haiku2 = await haikuist.fetchRandom();
+  const haiku3 = await haikuist.fetchRandom();
   t.ok(haiku1 && haiku1.id, `first haiku fetched: id=${haiku1.id}`);
   t.ok(haiku2 && haiku2.id, `second haiku fetched: id=${haiku2.id}`);
-  t.notEquals(haiku1.id, haiku2.id, 'haikus are different');
+  t.ok(haiku3 && haiku3.id, `third haiku fetched: id=${haiku3.id}`);
+  t.not(haiku1.id === haiku2.id === haiku3.id, 'haikus are different');
+});
+
+test('fetch about page', async t => {
+  t.plan(2);
+  const { title, content } = await haikuist.about();
+  t.equals(title, 'About', 'about page fetched');
+  t.ok(content && content.length > 256, `has content: length=${content.length}`);
 });
