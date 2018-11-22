f295ef7f1c6b9204b456980018a54cdb7ce3b728
Dario Vladovic <d.vladimyr@gmail.com>
2018-11-22 19:15:40 +0100

    Convert title & content to lazy properties :tada:
    - avoiding html2text execution if not necessary
    - using querystring#stringify for query creation

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 const debug = require('debug')('http');
 const html2text = require('html2plaintext');
 const r = require('got');
+const qs = require('querystring');
 const urlJoin = require('url-join');
 
 const API_URL = 'https://haiku.ist/wp-json/wp/v2/';
 const ABOUT = 10;
 const HAIKU = 2;
 
+const noop = Function.prototype;
+const lazy = getter => ({ enumerable: true, get: getter, set: noop });
 const normalize = url => url.replace(/\/$/, '');
 
 module.exports = {
@@ -35,16 +38,15 @@ async function about() {
   const url = urlJoin(API_URL, `/pages/${ABOUT}`);
   debug('url:', url);
   const { body: page = {} } = await r.get(url, { json: true });
-  return {
+  return Object.defineProperties({
     createdAt: page.date,
     modifiedAt: page.modified,
-    link: normalize(page.link),
-    ...parsePost(page)
-  };
+    link: normalize(page.link)
+  }, parsePost(page));
 }
 
 async function count() {
-  const query = `categories=${HAIKU}`;
+  const query = qs.stringify({ categories: HAIKU });
   const url = urlJoin(API_URL, `/posts?${query}`);
   debug('url:', url);
   const { headers } = await r.head(url);
@@ -52,26 +54,31 @@ async function count() {
 }
 
 async function fetchPosts({ pageSize = 10, offset = 0 } = {}) {
-  const query = `categories=${HAIKU}&per_page=${pageSize}&offset=${offset}`;
+  const query = qs.stringify({
+    status: 'publish',
+    categories: HAIKU,
+    per_page: pageSize,
+    offset: offset
+  });
   const url = urlJoin(API_URL, `/posts?${query}`);
   debug('url:', url);
   const { headers, body = [] } = await r.get(url, { json: true });
   const total = parseInt(headers['x-wp-total'], 10);
   const totalPages = parseInt(headers['x-wp-totalpages'], 10);
-  const posts = body.map(it => ({
+  const posts = body.map(it => Object.defineProperties({
     id: it.id,
     createdAt: it.date,
     modifiedAt: it.modified,
-    link: normalize(it.link),
-    ...parsePost(it)
-  }));
+    link: normalize(it.link)
+  }, parsePost(it)));
   return { total, totalPages, pageSize, posts };
 }
 
 function parsePost({ title, content } = {}) {
-  title = html2text(title.rendered);
-  content = html2text(content.rendered);
-  return { title, content };
+  return {
+    title: lazy(() => html2text(title.rendered)),
+    content: lazy(() => html2text(content.rendered))
+  };
 }
 
 function random(max) {
