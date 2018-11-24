03a08ab58e1e43d7d6508ad6458163d9ae47d089
Dario Vladovic <d.vladimyr@gmail.com>
2018-11-24 04:25:27 +0100

    Document public API :blue_book:

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const { config } = require('./package.json');
-const client = new (require('wp-api-client'))(config.baseUrl);
+const WordPressClient = require('wp-api-client');
+const client = new WordPressClient(config.baseUrl);
 
+/**
+ * Retrieve contents of [haiku.ist/about](https://haiku.ist/about/) page.
+ * @return {Promise<Page>} _About page._
+ */
 const about = () => client.fetchPage(config.aboutPage);
+/**
+ * Get total number of haikus.
+ * @return {Promise<Number>} _Total number of haikus available._
+ */
 const count = () => client.countPosts({ categories: config.haikus });
+/**
+ * Retrieve multiple haikus from [haiku.ist](https://haiku.ist) archive.
+ * @param  {Object} options WordPress REST API `/posts` endpoint
+ *                          [arguments](https://developer.wordpress.org/rest-api/reference/posts/#arguments).
+ * @param {Number} [options.pageSize=10] Maximum number of items to be returned in result set.
+ * @return {Promise<Response>} _Paginated listing of posts._
+ */
 const fetchPosts = options => client.fetchPosts({ ...options, categories: config.haikus });
 
 module.exports = {
@@ -15,11 +31,19 @@ module.exports = {
   fetchRandom
 };
 
+/**
+ * Fetch latest haiku.
+ * @return {Promise<Post>} _Latest post containing haiku._
+ */
 async function fetchLatest() {
   const { items: posts } = await fetchPosts({ pageSize: 1 });
   return posts[0];
 }
 
+/**
+ * Fetch random haiku from [haiku.ist](https://haiku.ist) archive.
+ * @return {Promise<Post>} _Random post containing haiku._
+ */
 async function fetchRandom() {
   const max = await count();
   const offset = random(max);
@@ -30,3 +54,23 @@ async function fetchRandom() {
 function random(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+/**
+ * WordPress API client `Page` response type.
+ * @name Page
+ * @alias Page WordPressClient.Item
+ * @see https://www.npmjs.com/package/wp-api-client#item
+ */
+/**
+ * WordPress API client `Post` response type.
+ * @name Post
+ * @alias Post WordPressClient.Item
+ * @see https://www.npmjs.com/package/wp-api-client#item
+ */
+
+/**
+ * WordPress API client response type used for paginated responses.
+ * @name Response
+ * @alias Response WordPressClient.Response
+ * @see https://www.npmjs.com/package/wp-api-client#response
+ */
